91d8d7ea0f697ad2adefe0ee7dd8fd35796035b1
anil-mndl <anilmandalmba@gmail.com>
2023-12-14 17:36:31 -0500

    updated naming conventions

--- api/real_file.py
+++ api/real_file.py
@@ -1,3 +1,8 @@
+# Issue 1
 # On Create Release step
 # Error: Error: Resource not accessible by integration
-# Solution: Repo > Settings > Action > General > Workflow permissions > Read and Write permissions > Save
+# Solution: Repo > Settings > Action > General > Workflow permissions >
+# Select 'Read and Write permissions' > Save
+
+
+# Issue 2
