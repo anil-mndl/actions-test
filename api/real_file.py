56b66c800298de719e3e9c20ad5331929672ed6f
anil-mndl <anilmandalmba@gmail.com>
2023-12-14 17:21:13 -0500

    troubleshooting Resource not accessible by integration issues

---
+++ api/real_file.py
@@ -0,0 +1,3 @@
+# On Create Release step
+# Error: Error: Resource not accessible by integration
+# Solution: Repo > Settings > Action > General > Workflow permissions > Read and Write permissions > Save
